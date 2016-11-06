84d8883cecec0598551687e3a28b28ff6cd0be41
nangelov1990 <nangelov1990@gmail.com>
2016-11-06 23:45:11 +0200

    Bug fixes

--- server/utils/generateRandomText.js
+++ server/utils/generateRandomText.js
@@ -1,7 +1,10 @@
 'use strict'
 const loremIpsum = require('lorem-ipsum')
 
-module.exports = (unit = 'sentence') => {
+module.exports = (unit) => {
+  if (!unit) {
+    unit = 'sentence.0'
+  }
   return loremIpsum({
     count: 1,
     units: unit,
